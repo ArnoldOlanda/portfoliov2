da99e168d2f915e8fe6abdb2f807aac02aa1e201
ArnoldOlanda <olanda188@gmail.com>
2024-03-29 20:09:37 -0500

    Actualizar importaciones y eliminar componente Nav

--- src/data/menuItems.ts
+++ src/data/menuItems.ts
@@ -1,18 +1,26 @@
 export const menuItems = [
-    {
-        text: "HOME",
-        icon: "/public/home.svg",
-    },
-    {
-        text: "ABOUT",
-        icon: "/public/user.svg",
-    },
-    {
-        text: "PORTFOLIO",
-        icon: "/public/portfolio.svg",
-    },
-    {
-        text: "CONTACT",
-        icon: "/public/imbox.svg",
-    },
+  {
+    id: 1,
+    text: 'HOME',
+    icon: '/public/home.svg',
+    url: '/',
+  },
+  {
+    id: 2,
+    text: 'ABOUT',
+    icon: '/public/user.svg',
+    url: '/about',
+  },
+  {
+    id: 3,
+    text: 'PORTFOLIO',
+    icon: '/public/portfolio.svg',
+    url: '/portfolio',
+  },
+  {
+    id: 4,
+    text: 'CONTACT',
+    icon: '/public/imbox.svg',
+    url: '/contact',
+  },
 ];
---
+++ src/data/projectItems.ts
@@ -0,0 +1,22 @@
+export const projectItems = [
+  {
+    id: 1,
+    title: 'Movies App',
+    img: '/src/assets/peliculas-app.png',
+  },
+  {
+    id: 2,
+    title: 'Youtube Redesign',
+    img: '/src/assets/youtube-redesign.png',
+  },
+  {
+    id: 3,
+    title: 'Repets Mobile App',
+    img: '/src/assets/repets-app.png',
+  },
+  {
+    id: 4,
+    title: 'Todo Mobile App',
+    img: '/src/assets/todo-app.png',
+  },
+];

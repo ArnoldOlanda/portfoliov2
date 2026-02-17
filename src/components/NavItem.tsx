876ecea4cd83f5f8b7e246952b58e68b30948bd9
ArnoldOlanda <olanda188@gmail.com>
2026-02-17 17:59:06 -0500

    Refactor layout and page components for consistency and readability
    - Updated import statements to use double quotes for consistency across files.
    - Adjusted layout components to improve responsiveness and visual structure.
    - Enhanced styling in About, Contact, Portfolio, and Skills pages for better alignment and spacing.
    - Added a Prettier configuration file for consistent code formatting.

--- src/components/NavItem.tsx
+++ src/components/NavItem.tsx
@@ -1,4 +1,3 @@
-
 interface Props {
   item: {
     id: number;
@@ -15,15 +14,15 @@ export const NavItem = ({ item, setActiveMenu, activeMenu }: Props) => {
     <a href={item.url} onClick={() => setActiveMenu(item.id)}>
       <div
         className={`${
-          activeMenu === item.id ? 'bg-blue-400' : 'bg-gray-800'
-        } group relative border-0 w-14 h-14 flex justify-center items-center rounded-full transition-all duration-300 hover:bg-blue-400`}
+          activeMenu === item.id ? "bg-blue-400" : "bg-gray-800"
+        } group relative flex h-14 w-14 items-center justify-center rounded-full border-0 transition-all duration-300 hover:bg-blue-400`}
       >
         <img
           src={item.icon}
           alt={item.text}
-          className="w-6 h-6 text-gray-800 dark:text-white"
+          className="h-6 w-6 text-gray-800 dark:text-white"
         />
-        <span className="absolute right-0 top-0 bg-blue-400 pl-6 pr-8 h-full opacity-0 group-hover:-translate-x-6 group-hover:opacity-100 group-hover:rounded-r-none transition-all duration-300 flex justify-center items-center rounded-full -z-10">
+        <span className="absolute right-0 top-0 -z-10 flex h-full items-center justify-center rounded-full bg-blue-400 pl-6 pr-8 opacity-0 transition-all duration-300 group-hover:-translate-x-6 group-hover:rounded-r-none group-hover:opacity-100">
           {item.text}
         </span>
       </div>
--- src/data/projectItems.ts
+++ src/data/projectItems.ts
@@ -1,107 +1,107 @@
-import peliculasApp from '/src/assets/peliculas-app.png';
-import repetsApp from '/src/assets/repets-app.png';
-import todoApp from '/src/assets/todo-app.png';
-import paymentsApp from '/src/assets/payments-app.png';
-import notesApp from '/src/assets/notes-app-vue-nest.png';
-import librarySystem from '/src/assets/library-system.png';
+import peliculasApp from "/src/assets/peliculas-app.png";
+import repetsApp from "/src/assets/repets-app.png";
+import todoApp from "/src/assets/todo-app.png";
+import paymentsApp from "/src/assets/payments-app.png";
+import notesApp from "/src/assets/notes-app-vue-nest.png";
+import librarySystem from "/src/assets/library-system.png";
 
 export const projectItems = [
   {
     id: 1,
-    title: 'Movies App',
+    title: "Movies App",
     img: peliculasApp,
     description:
-      'Aplicación web desarrollada con ReactJs y TypeScript que permite explorar y buscar películas con una interfaz moderna e intuitiva. Utilizando Tailwind CSS para un diseño responsivo y elegante, la aplicación se integra con The Movie Database API para obtener información actualizada en tiempo real. Los usuarios pueden navegar por películas populares, mejor valoradas y proximos estrenos, así como ver detalles completos de cada película, incluyendo sinopsis y trailers.',
+      "Aplicación web desarrollada con ReactJs y TypeScript que permite explorar y buscar películas con una interfaz moderna e intuitiva. Utilizando Tailwind CSS para un diseño responsivo y elegante, la aplicación se integra con The Movie Database API para obtener información actualizada en tiempo real. Los usuarios pueden navegar por películas populares, mejor valoradas y proximos estrenos, así como ver detalles completos de cada película, incluyendo sinopsis y trailers.",
     tecnologies: [
-      { name: 'ReactJs', bgClass: 'bg-blue-500/50' },
-      { name: 'Typescript', bgClass: 'bg-blue-600/50' },
-      { name: 'Tailwind CSS', bgClass: 'bg-cyan-500/50' },
-      { name: 'The Movie Database API', bgClass: 'bg-yellow-500/50' },
+      { name: "ReactJs", bgClass: "bg-blue-500/50" },
+      { name: "Typescript", bgClass: "bg-blue-600/50" },
+      { name: "Tailwind CSS", bgClass: "bg-cyan-500/50" },
+      { name: "The Movie Database API", bgClass: "bg-yellow-500/50" },
     ],
-    url: 'https://github.com/ArnoldOlanda/peliculas_app'
+    url: "https://github.com/ArnoldOlanda/peliculas_app",
   },
   {
     id: 2,
-    title: 'Repets Mobile App',
+    title: "Repets Mobile App",
     img: repetsApp,
     description:
-      'Aplicación móvil fullstack desarrollada con React Native CLI y TypeScript para el frontend, con un backend robusto en Express. Utiliza PostgreSQL para gestionar datos de mascotas y alojamiento de mascotas, Socket.IO para chat en tiempo real entre dueños de mascotas, Cloudinary para almacenamiento y optimización de imágenes, y Firebase para autenticación y notificaciones push. La arquitectura permite gestionar alojamientos, paseos y servicios veterinarios, facilitando la interacción entre usuarios y proveedores de servicios para mascotas.',
+      "Aplicación móvil fullstack desarrollada con React Native CLI y TypeScript para el frontend, con un backend robusto en Express. Utiliza PostgreSQL para gestionar datos de mascotas y alojamiento de mascotas, Socket.IO para chat en tiempo real entre dueños de mascotas, Cloudinary para almacenamiento y optimización de imágenes, y Firebase para autenticación y notificaciones push. La arquitectura permite gestionar alojamientos, paseos y servicios veterinarios, facilitando la interacción entre usuarios y proveedores de servicios para mascotas.",
     tecnologies: [
-      { name: 'React Native CLI', bgClass: 'bg-blue-400/50' },
-      { name: 'Express', bgClass: 'bg-gray-700/50' },
-      { name: 'PostgreSQL', bgClass: 'bg-blue-700/50' },
-      { name: 'Socket.IO', bgClass: 'bg-black/50' },
-      { name: 'TypeScript', bgClass: 'bg-blue-600/50' },
-      { name: 'Cloudinary', bgClass: 'bg-indigo-600/50' },
-      { name: 'Firebase', bgClass: 'bg-orange-500/50' },
+      { name: "React Native CLI", bgClass: "bg-blue-400/50" },
+      { name: "Express", bgClass: "bg-gray-700/50" },
+      { name: "PostgreSQL", bgClass: "bg-blue-700/50" },
+      { name: "Socket.IO", bgClass: "bg-black/50" },
+      { name: "TypeScript", bgClass: "bg-blue-600/50" },
+      { name: "Cloudinary", bgClass: "bg-indigo-600/50" },
+      { name: "Firebase", bgClass: "bg-orange-500/50" },
     ],
-    url: 'https://github.com/ArnoldOlanda/repetsApp'
+    url: "https://github.com/ArnoldOlanda/repetsApp",
   },
   {
     id: 3,
-    title: 'Todo Mobile App',
+    title: "Todo Mobile App",
     img: todoApp,
     description:
-      'Aplicación móvil de gestión de tareas desarrollada con React Native CLI y TypeScript, implementando Redux Toolkit para un manejo eficiente del estado global. El backend en Express se comunica con PostgreSQL para persistencia de datos, permitiendo sincronización entre dispositivos.',
+      "Aplicación móvil de gestión de tareas desarrollada con React Native CLI y TypeScript, implementando Redux Toolkit para un manejo eficiente del estado global. El backend en Express se comunica con PostgreSQL para persistencia de datos, permitiendo sincronización entre dispositivos.",
     tecnologies: [
-      { name: 'React Native CLI', bgClass: 'bg-blue-400/50' },
-      { name: 'Express', bgClass: 'bg-gray-700/50' },
-      { name: 'Typescript', bgClass: 'bg-blue-600/50' },
-      { name: 'Redux Toolkit', bgClass: 'bg-purple-600/50' },
-      { name: 'PostgreSQL', bgClass: 'bg-blue-700/50' },
+      { name: "React Native CLI", bgClass: "bg-blue-400/50" },
+      { name: "Express", bgClass: "bg-gray-700/50" },
+      { name: "Typescript", bgClass: "bg-blue-600/50" },
+      { name: "Redux Toolkit", bgClass: "bg-purple-600/50" },
+      { name: "PostgreSQL", bgClass: "bg-blue-700/50" },
     ],
-    url: 'https://github.com/ArnoldOlanda/todoAppTs-front'
+    url: "https://github.com/ArnoldOlanda/todoAppTs-front",
   },
   {
     id: 4,
-    title: 'Payments App',
+    title: "Payments App",
     img: paymentsApp,
     description:
-      'Aplicación móvil de pagos desarrollada con ReactJs, React Native y Expo para un desarrollo ágil multiplataforma, utilizando TypeScript en toda la stack. El backend en NestJS proporciona una arquitectura modular y escalable con inyección de dependencias, conectado a PostgreSQL para transacciones seguras. Zustand gestiona el estado de forma ligera y eficiente. Incluye gestion de creditos, manejo de pagos con fechas de vencimiento y lista de clientes divididos por zonas. La aplicacion cuenta con un panel administrativo web para la gestion de usuarios y reportes financieros.',
+      "Aplicación móvil de pagos desarrollada con ReactJs, React Native y Expo para un desarrollo ágil multiplataforma, utilizando TypeScript en toda la stack. El backend en NestJS proporciona una arquitectura modular y escalable con inyección de dependencias, conectado a PostgreSQL para transacciones seguras. Zustand gestiona el estado de forma ligera y eficiente. Incluye gestion de creditos, manejo de pagos con fechas de vencimiento y lista de clientes divididos por zonas. La aplicacion cuenta con un panel administrativo web para la gestion de usuarios y reportes financieros.",
     tecnologies: [
-      { name: 'React Native', bgClass: 'bg-blue-400/50' },
-      { name: 'Reactjs', bgClass: 'bg-blue-500/50' },
-      { name: 'Expo', bgClass: 'bg-slate-800/50' },
-      { name: 'Nestjs', bgClass: 'bg-red-600/50' },
-      { name: 'TypeScript', bgClass: 'bg-blue-600/50' },
-      { name: 'Redis', bgClass: 'bg-red-700/50' },
-      { name: 'Zustand', bgClass: 'bg-amber-600/50' },
-      { name: 'PostgreSQL', bgClass: 'bg-blue-700/50' },
+      { name: "React Native", bgClass: "bg-blue-400/50" },
+      { name: "Reactjs", bgClass: "bg-blue-500/50" },
+      { name: "Expo", bgClass: "bg-slate-800/50" },
+      { name: "Nestjs", bgClass: "bg-red-600/50" },
+      { name: "TypeScript", bgClass: "bg-blue-600/50" },
+      { name: "Redis", bgClass: "bg-red-700/50" },
+      { name: "Zustand", bgClass: "bg-amber-600/50" },
+      { name: "PostgreSQL", bgClass: "bg-blue-700/50" },
     ],
-    url: 'https://github.com/ArnoldOlanda/payments-app-api'
+    url: "https://github.com/ArnoldOlanda/payments-app-api",
   },
   {
     id: 5,
-    title: 'Notes App',
+    title: "Notes App",
     img: notesApp,
     description:
-      'Aplicación de notas desarrollada con Vue.js 3 y Pinia totalmente tipada con TypeScript. El backend en NestJS expone una API GraphQL que permite consultas flexibles y eficientes, reduciendo el over-fetching de datos. La arquitectura permite sincronización en tiempo real, búsqueda avanzada por contenido, etiquetas y categorias. La aplicacion ofrece multiples tipos de inicio de sesión via proveedores tales como Google y Github y autenticación tradicional',
+      "Aplicación de notas desarrollada con Vue.js 3 y Pinia totalmente tipada con TypeScript. El backend en NestJS expone una API GraphQL que permite consultas flexibles y eficientes, reduciendo el over-fetching de datos. La arquitectura permite sincronización en tiempo real, búsqueda avanzada por contenido, etiquetas y categorias. La aplicacion ofrece multiples tipos de inicio de sesión via proveedores tales como Google y Github y autenticación tradicional",
     tecnologies: [
-      { name: 'Vuejs', bgClass: 'bg-green-600/50' },
-      { name: 'Pinia', bgClass: 'bg-yellow-600/50' },
-      { name: 'Nestjs', bgClass: 'bg-red-600/50' },
-      { name: 'TypeScript', bgClass: 'bg-blue-600/50' },
-      { name: 'GraphQL', bgClass: 'bg-pink-600/50' },
-      { name: 'MySQL', bgClass: 'bg-blue-800/50' },
+      { name: "Vuejs", bgClass: "bg-green-600/50" },
+      { name: "Pinia", bgClass: "bg-yellow-600/50" },
+      { name: "Nestjs", bgClass: "bg-red-600/50" },
+      { name: "TypeScript", bgClass: "bg-blue-600/50" },
+      { name: "GraphQL", bgClass: "bg-pink-600/50" },
+      { name: "MySQL", bgClass: "bg-blue-800/50" },
     ],
-    url: 'https://github.com/ArnoldOlanda/notes-app-api'
+    url: "https://github.com/ArnoldOlanda/notes-app-api",
   },
   {
     id: 6,
-    title: 'Library System App',
+    title: "Library System App",
     img: librarySystem,
     description:
-      'Sistema de gestión de librerias y utiles de escritorio desarrollado con Reactjs y Shadcn UI. Utiliza Tanstack Query para la gestión eficiente de datos en el frontend. El backend en Nestjs ofrece una arquitectura robusta y escalable, mientras que Zustand maneja el estado global de la aplicación de manera ligera. Redis se emplea para mejorar el rendimiento mediante caching, y PostgreSQL garantiza una gestión segura y eficiente de la base de datos. La aplicación permite gestionar inventarios, ventas, compras, punto de venta y reportes detallados',
+      "Sistema de gestión de librerias y utiles de escritorio desarrollado con Reactjs y Shadcn UI. Utiliza Tanstack Query para la gestión eficiente de datos en el frontend. El backend en Nestjs ofrece una arquitectura robusta y escalable, mientras que Zustand maneja el estado global de la aplicación de manera ligera. Redis se emplea para mejorar el rendimiento mediante caching, y PostgreSQL garantiza una gestión segura y eficiente de la base de datos. La aplicación permite gestionar inventarios, ventas, compras, punto de venta y reportes detallados",
     tecnologies: [
-      { name: 'Reactjs', bgClass: 'bg-blue-500/50' },
-      { name: 'Shadcn UI', bgClass: 'bg-slate-900/50' },
-      { name: 'Tanstack Query', bgClass: 'bg-red-500/50' },
-      { name: 'Nestjs', bgClass: 'bg-red-600/50' },
-      { name: 'Zustand', bgClass: 'bg-amber-600/50' },
-      { name: 'Redis', bgClass: 'bg-red-700/50' },
-      { name: 'TypeScript', bgClass: 'bg-blue-600/50' },
-      { name: 'PostgreSQL', bgClass: 'bg-blue-700/50' },
+      { name: "Reactjs", bgClass: "bg-blue-500/50" },
+      { name: "Shadcn UI", bgClass: "bg-slate-900/50" },
+      { name: "Tanstack Query", bgClass: "bg-red-500/50" },
+      { name: "Nestjs", bgClass: "bg-red-600/50" },
+      { name: "Zustand", bgClass: "bg-amber-600/50" },
+      { name: "Redis", bgClass: "bg-red-700/50" },
+      { name: "TypeScript", bgClass: "bg-blue-600/50" },
+      { name: "PostgreSQL", bgClass: "bg-blue-700/50" },
     ],
-    url: 'https://github.com/ArnoldOlanda/library-system-api'
+    url: "https://github.com/ArnoldOlanda/library-system-api",
   },
 ];
